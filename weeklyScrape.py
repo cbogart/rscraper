463331065b5b51feab6c396895589b0449ce23dc
Chris Bogart <cbogartdenver@gmail.com>
2015-02-25 20:01:10 -0500

    saved change

--- weeklyScrape.py
+++ weeklyScrape.py
@@ -1,13 +1,22 @@
 import rscraper
 
 
+conn = getConnection()
 
 #update the listing of git projects
 #optionally, void git projects that have been touched, and not updated here recently
-biotasks = getBioconductorTasks()
-crantasks = getCranTasks()
+
+# Save information about the categorizations that the repos use
+createMetadataTables(conn)
+biometadata = getBioconductorMetadata()
 biopackages = getBioconductorPackageInfo()
 cranpackages = getCranPackageInfo()
-#update the listing of CRAN packages
-#update the listing of Bioconductor packages
+cranmetadata = getCranMetadata()
+saveMetadata(biopackages, biometadata, conn)
+saveMetadata(cranpackages, cranmetadata, conn)
+
+# Transfer specialized format of Git tables to 
+gitpackages = extractGitPackageInfo()
+gitmetadata = extractGitMetadata()
+saveMetadata(gitpackages, gitmetadata, conn)
 
